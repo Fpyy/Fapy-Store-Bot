74f067e56c7872f235b0ec40d314a4fd47fb1553
Fpyy_10 <medeiros.bernardo10@gmail.com>
2025-02-28 22:02:19 -0300

    Adiciona servidor HTTP para manter o bot online

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot está online!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
 import discord
 from discord import app_commands
 from discord.ext import commands
